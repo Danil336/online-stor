5508d0c43d8ba026108820ab9bb718f2160a993a
Danil336 <barsecdebil@gmail.com>
2022-08-23 17:16:24 +0200

    dkdkdkkd

--- src/alert.js
+++
@@ -1,8 +0,0 @@
-const goods = document.querySelector('.goods')
-
- for(let i = 0; i < goods.children.length; i++){
-    if(!(goods.children[i].classList.contains('hide') || goods.children[i].classList.contains('hide2') || goods.children[i].classList.contains('hide3') || goods.children[i].classList.contains('hide4') || goods.children[i].classList.contains('hide5'))){
-        continue
-    } else{
-    }
- }
